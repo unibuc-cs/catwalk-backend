19746f9b1e90a37a3f2afa20219b9e94229766f0
Alex Chirea <ac.chirea@gmail.com>
2020-10-30 16:17:08 +0200

    Intial commit

---
+++ src/main/java/com/catwalk/publicapicatwalk/CatWalkPublicApiApplication.java
@@ -0,0 +1,13 @@
+package com.catwalk.publicapicatwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatWalkPublicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatWalkPublicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catwalk/publicapicatwalk/CatWalkPublicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catwalk.publicapicatwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatWalkPublicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
