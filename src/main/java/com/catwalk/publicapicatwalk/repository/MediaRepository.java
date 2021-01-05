8fe45d8004e1273ce08d81a21c60070841ba019b
Alex Chirea <ac.chirea@gmail.com>
2021-01-05 18:33:43 +0200

    Admin Exercise

--- src/main/java/com/catwalk/publicapicatwalk/repository/MediaRepository.java
+++ src/main/java/com/catwalk/publicapicatwalk/repository/MediaRepository.java
@@ -1,6 +1,7 @@
 package com.catwalk.publicapicatwalk.repository;
 
 import com.catwalk.publicapicatwalk.model.Alimentation;
+import com.catwalk.publicapicatwalk.model.Exercise;
 import com.catwalk.publicapicatwalk.model.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import java.util.List;
 @Repository
 public interface MediaRepository extends JpaRepository<Media, String> {
     List<Media> findByAlimentation(Alimentation a);
+    List<Media> findByExercise(Exercise e);
 }
